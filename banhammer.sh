02219e48bdcfd28d087ce852037ab995f91aa2cf
ibarria0 <ibarria0@gmail.com>
2014-01-04 01:28:08 -0500

    i can spam too

--- banhammer.sh
+++ banhammer.sh
@@ -6,17 +6,20 @@ else
    exit 1
 fi
 
-for IP in $(netstat -ntu | grep ":80" | awk '{print $5}'| cut -d: -f1 | sort | uniq -c | sort -n | grep -v 127.0.0.1 | awk -v connlimit=$CONNLIMIT '{if ($1 > connlimit) print $2;}')
+for i in {1..60}
 do
-    if iptables --list -n | grep DROP | awk '{print $4}' | grep -q "$IP"
-    then
-	for PID in $(netstat -ntup | grep "$IP" | awk '{print $7}'| cut -d/ -f1 | grep -v "-" | sort | uniq )
+	for IP in $(netstat -ntu | grep ":80" | awk '{print $5}'| cut -d: -f1 | sort | uniq -c | sort -n | grep -v 127.0.0.1 | awk -v connlimit=$CONNLIMIT '{if ($1 > connlimit) print $2;}')
 	do
-		kill -9 $PID
+	    if iptables --list -n | grep DROP | awk '{print $4}' | grep -q "$IP"
+	    then
+		for PID in $(netstat -ntup | grep "$IP" | awk '{print $7}'| cut -d/ -f1 | grep -v "-" | sort | uniq )
+		do
+			kill -9 $PID
+		done
+	    else
+		echo "banning $IP"
+		iptables -A INPUT -s $IP -j DROP
+	    fi
 	done
-    else
-	echo "banning $IP"
-	iptables -A INPUT -s $IP -j DROP
-    fi
 done
 
