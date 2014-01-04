66f552f64199e94856c5ef8aa09f1940324be26b
ibarria0 <ibarria0@gmail.com>
2014-01-04 01:26:13 -0500

    kill them conns

--- banhammer.sh
+++ banhammer.sh
@@ -5,15 +5,18 @@ else
    echo "Usage: $0 connection_limit"
    exit 1
 fi
+
 for IP in $(netstat -ntu | grep ":80" | awk '{print $5}'| cut -d: -f1 | sort | uniq -c | sort -n | grep -v 127.0.0.1 | awk -v connlimit=$CONNLIMIT '{if ($1 > connlimit) print $2;}')
 do
     if iptables --list -n | grep DROP | awk '{print $4}' | grep -q "$IP"
     then
-        echo "$IP already banned"
+	for PID in $(netstat -ntup | grep "$IP" | awk '{print $7}'| cut -d/ -f1 | grep -v "-" | sort | uniq )
+	do
+		kill -9 $PID
+	done
     else
-        echo "banning $IP"
+	echo "banning $IP"
 	iptables -A INPUT -s $IP -j DROP
-        echo "$IP" >> banned.txt
     fi
 done
 
